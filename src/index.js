16a1105e0fce9b8577115bec68b26d28167197e8
pasha <pasha.colocov@gmail.com>
2020-07-04 11:23:02 +0300

    hello

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const event = require('events');
+const readline = require('readline');
+
+const emitter = new event();
+
+const interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+emitter.on('message', () => console.log('message is work'));
+
+emitter.on('email', () => console.log('email is work'));
+
+while (true) {
+interface.question('enter 1="message" or 0="email"', (data) => {
+    data === 1 ? emitter.emit('message') : emitter.emit('email');
+    });
+};
+
+
+
+
+
